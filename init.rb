e2e4f26330059376d0d9212776f4b7c80665bb9f
Igor Zubkov <igor.zubkov@gmail.com>
2012-03-19 14:55:47 +0200

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_better_imap do
-  name 'Redmine Better Imap plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Redmine Better IMAP plugin'
+  author 'Igor Zubkov'
+  description 'Better IMAP plugin for redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/biow0lf/redmine_better_imap'
+  author_url 'https://github.com/biow0lf'
 end
