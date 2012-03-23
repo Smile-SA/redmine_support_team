7beb96e446612c82bf7f273cc0b8e60cb337551e
Igor Zubkov <igor.zubkov@gmail.com>
2012-03-23 12:00:27 +0200

    Rescue more exceptions

--- app/models/bug_mail.rb
+++ app/models/bug_mail.rb
@@ -17,23 +17,32 @@ class BugMail
 
       begin
         imap = Net::IMAP.new(c['host'], c['port'], c['ssl'], nil, false)
-
-        begin
-          imap.login(c['username'], c['password'])
-        rescue Net::IMAP::NoResponseError
-          puts "Login or password incorrect"
-        end
-
-        begin
-          imap.examine(c['folder'])
-          imap.select(c['folder'])
-        rescue Net::IMAP::NoResponseError
-          puts "Can't select folder"
-        end
-        imap.disconnect
-      rescue
-        puts "Bad"
+      rescue Errno::ECONNREFUSED,        # connection refused by host or an intervening firewall.
+             Errno::ETIMEDOUT,           # connection timed out (possibly due to packets being dropped by an intervening firewall).
+             Errno::ENETUNREACH,         # there is no route to that network.
+             SocketError,                # hostname not known or other socket error.
+             Net::IMAP::ByeResponseError # we connected to the host, but they immediately said goodbye to us.
+        puts "Can't connect to IMAP server"
+        next
       end
+
+      begin
+        imap.login(c['username'], c['password'])
+      rescue Net::IMAP::NoResponseError
+        puts "Login or password incorrect"
+        next
+      end
+
+      begin
+        imap.examine(c['folder'])
+        imap.select(c['folder'])
+      # A Net::IMAP::NoResponseError is raised if the mailbox does not exist
+      # or is for some reason non-examinable
+      rescue Net::IMAP::NoResponseError
+        puts "Can't select folder"
+        next
+      end
+      imap.disconnect
 
       # imap.search(['NOT', 'SEEN']).each do |message_id|
       #   msg = imap.fetch(message_id,'RFC822')[0].attr['RFC822']
